438135f4996ad91b77d7231ad41a2dfdf981cb3f
losthermes <43258126+losthermes@users.noreply.github.com>
2019-08-16 14:47:50 -0700

    Update settings.py

--- FOI/settings.py
+++ FOI/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY =
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pacific-eyrie-53047.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['', 'localhost']
 
 
 # Application definition
